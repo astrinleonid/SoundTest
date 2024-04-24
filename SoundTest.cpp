eaf86fd069155e8e0c9a709aab2daaf691616c28
Leonid Astrin <astrinleonid@gmail.com>
2024-04-24 15:53:29 +0300

    working two threads with on the go change of freaquncy

--- SoundTest.cpp
+++ SoundTest.cpp
@@ -8,10 +8,11 @@
 #include <cmath> // For M_PI
 #include <thread>
 #include <limits>
-const int buffersize = 96;
+const int buffersize = 48;
 struct ToneGeneratorState {
     std::atomic<float> frequency{ 440.0 }; // Default frequency (A4)
-};
+    std::atomic<bool> applicationIsRunning{ true };
+ };
 
 class CircularBuffer {
 public:
@@ -19,34 +20,39 @@ public:
 
     void produce(const Sint16* data, size_t samples) {
         std::unique_lock<std::mutex> lock(mutex);
+        // Wait until there is enough space to produce new data
+        canProduce.wait(lock, [&] { return size + samples <= capacity; });
         for (size_t i = 0; i < samples; ++i) {
-            if (size < capacity) { // Avoid overwriting data that hasn't been consumed
-                buffer[end] = data[i];
-                end = (end + 1) % capacity;
-                ++size;
-            }
+            buffer[end] = data[i];
+            end = (end + 1) % capacity;  // Wrap around at the end of the buffer
+            ++size;
         }
-        dataAvailable.notify_one();
+        lock.unlock();
+        canConsume.notify_one(); // Notify consumer that data is available
     }
 
     void consume(Sint16* data, size_t samples) {
         std::unique_lock<std::mutex> lock(mutex);
-        dataAvailable.wait(lock, [&] { return size >= samples; }); // Ensure enough data is available
+        // Wait until there is enough data to consume
+        canConsume.wait(lock, [&] { return size >= samples; });
         for (size_t i = 0; i < samples; ++i) {
             data[i] = buffer[begin];
-            begin = (begin + 1) % capacity;
+            begin = (begin + 1) % capacity;  // Wrap around at the beginning of the buffer
             --size;
         }
+        lock.unlock();
+        canProduce.notify_one(); // Notify producer that space is available
     }
 
 private:
     std::vector<Sint16> buffer;
     std::mutex mutex;
-    std::condition_variable dataAvailable;
+    std::condition_variable canProduce, canConsume;
     size_t begin, end, size, capacity;
 };
 
-CircularBuffer audioBuffer(48000 * 2);
+
+CircularBuffer audioBuffer(buffersize * 2);
 
 void audioCallback(void* userdata, Uint8* stream, int len) {
     int samplesNeeded = len / sizeof(Sint16);
@@ -58,15 +64,17 @@ void generateWave(CircularBuffer& buffer, ToneGeneratorState& state) {
     const int sampleRate = 48000;
     std::vector<Sint16> sampleBuffer(buffersize);
 
-    while (true) { // Keep running indefinitely
+    while (state.applicationIsRunning.load()) { // Keep running indefinitely
+       
         double frequency = state.frequency.load();
         double phaseStep = frequency * 2 * M_PI / sampleRate;
-        for (int i = 0; i < 48; ++i) {
-            sampleBuffer[i] = static_cast<Sint16>(sin(phase) * 32767);
+        for (int i = 0; i < buffersize; ++i) {
+        sampleBuffer[i] = static_cast<Sint16>(sin(phase) * 32767);
             phase += phaseStep;
             if (phase > 2 * M_PI) phase -= 2 * M_PI;
         }
         buffer.produce(sampleBuffer.data(), buffersize);
+     
     }
 }
 
@@ -81,7 +89,7 @@ int main(int argc, char* argv[]) {
     want.freq = 48000;
     want.format = AUDIO_S16SYS;
     want.channels = 2;
-    want.samples = 4096;
+    want.samples = buffersize;
     want.callback = audioCallback;
 
     SDL_AudioDeviceID dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
@@ -90,14 +98,14 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
-
+    
     SDL_PauseAudioDevice(dev, 0); // Start the audio device
 
     ToneGeneratorState toneState;
     std::thread waveThread(generateWave, std::ref(audioBuffer), std::ref(toneState));
 
-    bool applicationIsRunning = true;
-    while (applicationIsRunning) {
+   
+    while (toneState.applicationIsRunning.load()) {
         std::cout << "Enter 'u' to increase tone, 'd' to decrease, 'q' to quit: ";
         char command;
         std::cin >> command;
@@ -117,7 +125,7 @@ int main(int argc, char* argv[]) {
             toneState.frequency.store(toneState.frequency.load() - 10.0f);
             break;
         case 'q':
-            applicationIsRunning = false;
+            toneState.applicationIsRunning.store(false);
             break;
         }
     }
